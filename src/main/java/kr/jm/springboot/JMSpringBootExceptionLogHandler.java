73b0a1bec92b47cd905be8fd569f4b86a2b8973e
허제민(Jemin Huh)/System Software 개발팀/SKP <jemin.huh@sk.com>
2016-07-15 14:07:56 +0900

    JMStringBootExceptionLogHandler 추가 - springboot 내부 exception을 error log 로 관리

---
+++ src/main/java/kr/jm/springboot/JMSpringBootExceptionLogHandler.java
@@ -0,0 +1,21 @@
+package kr.jm.springboot;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+import kr.jm.utils.exception.JMExceptionManager;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@ControllerAdvice
+public class JMSpringBootExceptionLogHandler {
+	@ExceptionHandler(Exception.class)
+	public ModelAndView logErrorHandler(HttpServletRequest req, Exception e)
+			throws Exception {
+		JMExceptionManager.logException(log, e, "logErrorHandler", req);
+		throw e;
+	}
+}
